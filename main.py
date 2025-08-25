2fc7f1ea82887c927648a6139e7c6874af7ff6ef
George-Okumu <gokumu12@gmail.com>
2025-08-25 22:31:19 +0300

    new ui flow

--- main.py
+++ main.py
@@ -15,8 +15,30 @@ def main():
         print("Login failed:", e)
         return
 
-    print(f"Welcome {current_user.username} ({current_user.role})")
+    print(f"Welcome {current_user[1]} to the ({current_user[3]}) portal")
     
+    while True:
+        if current_user[3] == 'admin':
+            print("                                            ")
+            print("What do you want to achieve today?")
+            
+            print("\n1. Create a librarian")
+            print("2. Add a user")
+            print("3. Add a student")
+            print("4. Exit")
+            
+            choice = input("  :")
+            
+            if choice == "2":
+                username = input("Enter users username: ")
+                password = input("Create a unique password for the user: ")
+                role = input("Assign role to the user: ")
+                
+                user = User(username, password, role)
+                
+                UserService.add_user(user)
+            else:
+                break
         
 if __name__ == "__main__":
     main()    
